aa6465e8da4783a0e7ab80da25d27cd487fee281
robshepard <33606148+robshepard@users.noreply.github.com>
2022-05-31 19:06:54 +0200

    Add files via upload

---
+++ scripts/Tab_Script.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+var selected = false
+var touchpos = 0
+
+
+func _physics_process(delta):
+	if selected:
+		global_position = lerp(global_position, touchpos, 25 * delta)
+	
+
+func _on_CloseBox_input_event(_viewport, event, _shape_idx): #muss eventuell umbenannt werden
+	if event is InputEventScreenTouch:
+		self.visible = false
+	if Input.is_action_just_pressed("press"):
+		self.visible =false
+
+func make_visible():
+	self.visible = true
+
+
+func _on_MoveBox_input_event(_viewport, event, _shape_idx):
+	if event is InputEventScreenTouch:
+		print("touch")
+		if event.is_pressed():
+			print("pressed")
+			selected = true
+		
+	if selected:
+		touchpos = event.position
+		
+	if event is InputEventScreenTouch:
+		if not event.is_pressed():
+			selected = false
+		
