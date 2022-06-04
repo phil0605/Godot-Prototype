e1b5584f25d0be2ca195e54f1fe043f5c6681769
phil0605 <preiplinger@yahoo.de>
2022-06-04 14:21:03 +0200

    Add time feature
    Added time visualization for both sides

--- Tutorial/Tutorial.gd
+++ Tutorial/Tutorial.gd
@@ -4,15 +4,22 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var time
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	time = 0
+	$ScoreTimer.start()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	time = time + delta
+	
+	var seconds:float = fmod(time , 60.0)
+	var minutes:int   = int(time / 60.0) % 60
+	
+	$TimeLabelLeft.text = str("%02d:%02d" % [minutes, seconds])
+	$TimeLabelRight.text = str("%02d:%02d" % [minutes, seconds])
 
 	
