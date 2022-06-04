a54f55ba40d4d0e1e9fb41740bdf09b798d0d38a
phil0605 <preiplinger@yahoo.de>
2022-06-04 17:41:44 +0200

    Add validation and trials

--- scripts/Input_field.gd
+++ scripts/Input_field.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-
-
-
 var password = ""
+var trials = 3
 
+func _ready():
+	$Trials.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -18,25 +18,54 @@ func _on_Button_pressed():
 
 func _on_Minimize_pressed():
 	var tab = $ColorRect.get_parent()
-	tab.visible = false # Replace with function body.
-	
-	#var children = $ColorRect.get_children()
-	
-	#for child in children.size():
-		#$ColorRect.get_child(child).hide()
-
+	tab.visible = false
 
 func _on_Button_entf_pressed():
 	password.erase(password.length()-1,1)
 
 
 func _on_Button_enter_pressed():
-	return password
+	var tutorial_node = get_parent().get_parent().get_parent()
+	var solution = tutorial_node.getPassword()
+	
+	if(solution == password):
+		var tab = $ColorRect.get_parent()
+		tab.visible = false
+		$Trials.clear()
+		var message = "Ihr habt das Level geschafft"
+		tutorial_node.setCongratMessage(message)
+		
+	else:
+		$Trials.clear()
+		$Trials.add_text("Das war leider falsch. Versuch es gerne nochmal. Du hast noch ")
+		$Trials.push_color(Color.green)
+		$Trials.add_text(str(trials))
+		$Trials.push_color(Color.red)
+		$Trials.add_text(" Versuche übrig. Für jeden Versuch gibt es 10 Sekunden Strafe.")
+		$Trials.visible = true
+		$TrialTimer.start()
+		
+		if(trials == 1):
+			$Trials.add_text("Die Lösung versteckt sich in einem Instagrampost, der sein Geburtsdatum als Passwort benutzt")
+			
+		if(trials == 0):
+			password = solution
+			var time = tutorial_node.getTime()
+			time += 60
+			tutorial_node.setTime(time)
+			$TrialTimer.stop()
+			$Trials.clear()
+			$Trials.add_text("Da du keine Versuche mehr hast gibt es eine Minute Strafe. Drücke ")
+			$Trials.push_color(Color.green)
+			$Trials.add_text("Enter")
+			$Trials.push_color(Color.red)
+			$Trials.add_text(" um fortzufahren")
+			
+		trials = trials - 1
 
 
 func _on_Button_1_pressed():
 	password = password + "1"
-
 
 func _on_Button_2_pressed():
 	password = password + "2"
@@ -87,3 +116,7 @@ func make_visible():
 	var tab = $ColorRect.get_parent()
 	tab.visible = true
 
+func _on_TrialTimer_timeout():
+	$Trials.visible = false
+	$Trials.clear()
+	
--- scripts/Tutorial.gd
+++ scripts/Tutorial.gd
@@ -5,11 +5,15 @@ extends Node
 # var a = 2
 # var b = "text"
 var time
+var insta_password = "17051997"
+var congrat_message = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	time = 0
 	$ScoreTimer.start()
+	$CongratulationLabelLeft.visible = false
+	$CongratulationLabelRight.visible = false
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,5 +25,22 @@ func _process(delta):
 	
 	$TimeLabelLeft.text = str("%02d:%02d" % [minutes, seconds])
 	$TimeLabelRight.text = str("%02d:%02d" % [minutes, seconds])
-
 	
+	$CongratulationLabelLeft.text = congrat_message
+	$CongratulationLabelRight.text = congrat_message
+	
+	
+
+func getPassword():
+	return insta_password
+	
+func setTime(new_time):
+	time = new_time
+
+func getTime():
+	return time
+	
+func setCongratMessage(new_congrat_message):
+	congrat_message = new_congrat_message
+	$CongratulationLabelLeft.show()
+	$CongratulationLabelRight.show()
