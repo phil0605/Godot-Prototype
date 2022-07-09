b8c3d5dfcc45cf45c0b249f517df0aed8302baf3
Maurice Donat <maurice.donat@mailbox.tu-dresden.de>
2022-07-09 13:10:48 +0200

    ComicEnde beim Tutorial nur, wenn Fall gelöst

--- scripts/Levelende.gd
+++ scripts/Levelende.gd
@@ -2,6 +2,7 @@ extends ColorRect
 
 
 var is_paused = false setget set_is_paused
+var inTime
 
 
 func set_is_paused(value):
@@ -14,16 +15,21 @@ func show_end(time):
 	if (time >= 20*60):
 		$CenterContainer/Menu1/Ergebnis.text = "Zeitüberschreitung!"
 		$CenterContainer/Menu1/Text.text = "Score 0"
+		inTime = false
 	else:
 		var score = 1000 - (int(int(time) / 6) * 5)
 		var text = "Score " + str(score)
 		$CenterContainer/Menu1/Ergebnis.text = "Fall gelöst!"
 		$CenterContainer/Menu1/Text.text = text
+		inTime = true
 	self.visible = true
 	
 
 func _on_QuitButton_pressed():
-	SceneTransition.change_scene("res://scenes/Comic1Ende.tscn")
+	if (inTime):
+		SceneTransition.change_scene("res://scenes/Comic1Ende.tscn")
+	else:
+		SceneTransition.change_scene("res://scenes/hauptmenu.tscn")
 	self.is_paused = false
 	#self.visible = false
 
